4f58cd9399e06523368a34978d2b99b54a1bf053
Diwas Atreya <diwasatreya45@gmail.com>
2025-11-14 21:50:50 +0545

    Fixed issue in api middleware

--- src/middlewares/api.middleware.js
+++ src/middlewares/api.middleware.js
@@ -3,8 +3,8 @@ import { verifyAPIKey } from "../services/api.services.js";
 const checkAPI = async (req, res, next) => {
     try {
         const authorization = req.headers.authorization;
-        if (!authorization && !authorization.includes('Bearer')) {
-            return res.status(401).json({ error: "Invalid Authorization Format!", success: false });
+        if (typeof authorization == 'undefined' || !authorization.includes('Bearer')) {
+            return res.status(401).json({ error: "Permission Declined! No Authorization!", success: false });
         }
         const apiKey = authorization.replace('Bearer ', '');
         if (!apiKey) {
